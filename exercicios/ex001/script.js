b7f0408a00e0ed87ddfe182f6056bce834867861
vandesonsantos <92255361+vandesonsantos@users.noreply.github.com>
2021-11-23 15:26:06 -0300

    Atualização do Code

--- exercicios/ex001/script.js
+++ exercicios/ex001/script.js
@@ -1,24 +1,24 @@
 function carregar() {
-    var msg = window.document.getElementById('msg')
-    var imagem = window.document.getElementById('imagem')
+    var mensagem = window.document.getElementById('msg') 
+    var imagemhtml = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    //var hora = 23
-    msg.innerHTML = `Agora são ${hora} horas.`
+    //var hora = 9
+    mensagem.innerHTML = `Agora são ${hora} horas.`
     
     if (hora >= 0 && hora < 12) {
         //BOM DIA!
-        imagem.src = "imagens/manha.png"
+        imagemhtml.src = "imagens/manha.png"
         document.body.style.background = 'var(--manha)'
 
-    } else if (hora >=12 && hora <= 18) {
+    } else if (hora >=12 && hora < 18) {
         //BOA TARDE!
-        imagem.src = "imagens/tarde.png"
+        imagemhtml.src = "imagens/tarde.png"
         document.body.style.background = 'var(--tarde)'
 
     } else {
         //BOA NOITE!
-        imagem.src = "imagens/noite.png"
+        imagemhtml.src = "imagens/noite.png"
         document.body.style.background = 'var(--noite)'
     }
 }
