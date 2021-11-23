ee555c20d4ad588e83064d3d70c189f264b2306d
vandesonsantos <92255361+vandesonsantos@users.noreply.github.com>
2021-11-23 14:54:03 -0300

    atualizado

--- exercicios/ex001/script.js
+++ exercicios/ex001/script.js
@@ -2,9 +2,10 @@ function carregar() {
     var msg = window.document.getElementById('msg')
     var imagem = window.document.getElementById('imagem')
     var data = new Date()
-    //var hora = data.getHours()
-    var hora = 23
+    var hora = data.getHours()
+    //var hora = 23
     msg.innerHTML = `Agora são ${hora} horas.`
+    
     if (hora >= 0 && hora < 12) {
         //BOM DIA!
         imagem.src = "imagens/manha.png"
